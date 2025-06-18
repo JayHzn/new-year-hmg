d2da1e6d98313c42226ce6bf159abdf78ba0e3a1
JayEpic <117071162+JayHzn@users.noreply.github.com>
2025-06-18 21:03:05 +0200

    Login updated

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -11,8 +11,8 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/signin',
-      name: 'signin',
+      path: '/register',
+      name: 'register',
       component: SignInView,
     },
     {
