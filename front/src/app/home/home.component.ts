8b6ebe8007070600200f89c2438075436d73271c
JayEpic <117071162+JayHzn@users.noreply.github.com>
2025-06-26 11:46:56 +0200

    Migrate to ngModule

--- front/src/app/home/home.component.ts
+++ front/src/app/home/home.component.ts
@@ -1,23 +1,11 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../auth/auth.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
+  standalone: false,
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
+export class HomeComponent {
 
-export class HomeComponent implements OnInit {
-  users: any[] = [];
-
-  constructor(private auth: AuthService, private http: HttpClient) {}
-
-  ngOnInit() {
-      this.http.get<any[]>('/api/users').subscribe(data => this.users = data);
-  }
-
-  logout() {
-    this.auth.logout();
-  }
 }
--- front/src/app/login/login.component.ts
+++ front/src/app/login/login.component.ts
@@ -1,23 +1,11 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-login',
+  standalone: false,
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  email = '';
-  password = '';
-  error = '';
 
-  constructor(private auth: AuthService, private router: Router) {}
-
-  onSubmit() {
-    this.auth.login(this.email, this.password).subscribe({
-      next: () => this.router.navigate(['/home']),
-      error: () => this.error = 'Login failed'
-    })
-  }
 }
